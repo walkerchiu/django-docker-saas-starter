952e1ce8846f24d53f7c77db1531f1589d7c38ec
walkerchiu <chenjen.chiou@gmail.com>
2022-09-11 08:25:05 +0800

    Feat(API): Add a shared entry for login

--- app/core/graphql_jwt/relay.py
+++ app/core/graphql_jwt/relay.py
@@ -1,10 +1,15 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.db import connection
 
+from django_tenants.utils import schema_context
+from graphene.utils.thenables import maybe_thenable
 from graphql_jwt.refresh_token.relay import DeleteRefreshTokenCookie, Revoke
 import graphene
 
 from core.graphql_jwt import mixins
 from core.graphql_jwt.decorators import token_auth
+from tenant.models import Tenant
 
 __all__ = [
     "JSONWebTokenMutation",
@@ -32,6 +37,38 @@ class JSONWebTokenMutation(mixins.ObtainJSONWebTokenMixin, graphene.ClientIDMuta
             },
         )
         return super().Field(*args, **kwargs)
+
+    @classmethod
+    def mutate(cls, root, info, input):
+        def on_resolve(payload):
+            try:
+                payload.client_mutation_id = input.get("client_mutation_id")
+            except Exception:
+                raise Exception(
+                    f"Cannot set client_mutation_id in the payload object {repr(payload)}"
+                )
+            return payload
+
+        schema_name = connection.schema_name
+
+        if (
+            info.context.headers.get("X-Tenant")
+            == "account" + "." + settings.APP_DOMAIN
+        ):
+            with schema_context(settings.PUBLIC_SCHEMA_NAME):
+                tenant = (
+                    Tenant.objects.filter(email=input.get("email"))
+                    .order_by("created_at")
+                    .first()
+                )
+                if tenant:
+                    schema_name = tenant.schema_name
+                else:
+                    raise Exception("Please enter valid credentials")
+
+        with schema_context(schema_name):
+            result = cls.mutate_and_get_payload(root, info, **input)
+            return maybe_thenable(result, on_resolve)
 
     @classmethod
     @token_auth
