46beff52d6203651753c8fdd05ca764af8ee2001
walkerchiu <chenjen.chiou@gmail.com>
2023-03-15 20:22:02 +0800

    Feat(Function): Update updateEmail in TenantService

--- app/tenant/services/tenant_service.py
+++ app/tenant/services/tenant_service.py
@@ -3,11 +3,12 @@ import uuid
 
 from django.conf import settings
 from django.db import transaction
+from django.db.models import Exists, OuterRef
 from django.db.utils import IntegrityError
 
 from safedelete.models import HARD_DELETE
 
-from tenant.models import Contract, Tenant
+from tenant.models import Contract, Domain, Tenant
 from tenant.services.domain_service import DomainService
 
 
@@ -51,10 +52,29 @@ class TenantService:
         return result, tenant
 
     @transaction.atomic
-    def updateEmail(self, email_original: str, email_new: str) -> bool:
-        Tenant.all_objects.filter(email=email_original).update(email=email_new)
+    def updateEmail(self, scope: str, email_original: str, email_new) -> bool:
+        domain_hq = "hq." + settings.DOMAIN_HQ
 
-        return True
+        match scope:
+            case "all":
+                Tenant.all_objects.filter(email=email_original).update(email=email_new)
+                return True
+            case "hq":
+                Tenant.all_objects.filter(email=email_original).filter(
+                    Exists(
+                        Domain.objects.filter(tenant=OuterRef("pk"), domain=domain_hq)
+                    )
+                ).update(email=email_new)
+                return True
+            case "organization":
+                Tenant.all_objects.filter(email=email_original).exclude(
+                    Exists(
+                        Domain.objects.filter(tenant=OuterRef("pk"), domain=domain_hq)
+                    )
+                ).update(email=email_new)
+                return True
+
+        return False
 
     @transaction.atomic
     def delete_tenant(self, tenant_id: uuid) -> bool:
