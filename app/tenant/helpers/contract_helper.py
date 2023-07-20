85f0305a5b0657b1ac31eea543dc9d7ccde3141d
walkerchiu <chenjen.chiou@gmail.com>
2023-07-20 20:22:38 +0800

    Feat(Function): Add the judgment that the time field is null to the check_if_it_is_within_the_validity_period function

--- app/tenant/helpers/contract_helper.py
+++ app/tenant/helpers/contract_helper.py
@@ -1,4 +1,5 @@
 from django.db import connection
+from django.db.models import Q
 from django.utils.timezone import datetime
 
 from tenant.models import Contract
@@ -13,6 +14,9 @@ class ContractHelper:
     ) -> bool:
         return (
             Contract.objects.filter(tenant__schema_name=self.schema_name)
-            .filter(effective_from__lt=datetime.now(), expired_on__gt=datetime.now())
+            .filter(
+                Q(effective_from__isnull=True) | Q(effective_from__lt=datetime.now()),
+                Q(expired_on__isnull=True) | Q(expired_on__gt=datetime.now()),
+            )
             .exists()
         )
