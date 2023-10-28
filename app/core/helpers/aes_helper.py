cabf634f8ab05b4bc1a4b9415289ee02439bee90
walkerchiu <chenjen.chiou@gmail.com>
2023-10-28 08:11:40 +0800

    Feat(Module): Switch encryption mode from CBC to GCM

--- app/core/helpers/aes_helper.py
+++ app/core/helpers/aes_helper.py
@@ -7,13 +7,13 @@ from urllib.parse import quote, unquote
 
 class AESHelper:
     def __init__(self, key: str, iv: str):
-        self.key = key.encode("utf-8")
-        self.iv = iv.encode("utf-8")
+        self.key = bytes.fromhex(key)
+        self.iv = bytes.fromhex(iv)
 
     def encrypt_data(self, data):
         data = quote(data)
         data = data.encode("utf-8")
-        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        cipher = AES.new(self.key, AES.MODE_GCM, self.iv)
         ct_bytes = cipher.encrypt(pad(data, AES.block_size))
         ct_base64 = base64.b64encode(ct_bytes).decode("utf-8")
 
@@ -21,7 +21,7 @@ class AESHelper:
 
     def decrypt_data(self, data):
         ct_bytes = base64.b64decode(data)
-        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+        cipher = AES.new(self.key, AES.MODE_GCM, self.iv)
         pt_bytes = unpad(cipher.decrypt(ct_bytes), AES.block_size)
         pt = pt_bytes.decode("utf-8")
         pt = unquote(pt)
