47f0ff107fdc3dedb3f458444f61eed288f60e33
walkerchiu <chenjen.chiou@gmail.com>
2023-10-31 20:08:23 +0800

    Feat(Function): Add generate_hmac to KMSHelper

--- app/core/helpers/kms_helper.py
+++ app/core/helpers/kms_helper.py
@@ -9,9 +9,11 @@ class KMSHelper:
     def __init__(
         self,
         key_id: str = settings.AWS_KMS_KEY_ID,
+        key_id_hmac: str = settings.AWS_KMS_KEY_ID_HMAC,
         region_name: str = settings.AWS_KMS_REGION_NAME,
     ):
         self.key_id = key_id
+        self.key_id_hmac = key_id_hmac
         self.client = boto3.client("kms", region_name=region_name)
 
     def encrypt(
@@ -44,3 +46,18 @@ class KMSHelper:
         plaintext = response["Plaintext"].decode("utf-8")
 
         return plaintext
+
+    def generate_hmac(
+        self, plaintext: str, algorithm: str = "HMAC_SHA_256", key_id: str = None
+    ) -> str:
+        if key_id is None:
+            key_id = self.key_id_hmac
+
+        response = self.client.generate_mac(
+            KeyId=key_id,
+            Message=plaintext.encode("utf-8"),
+            MacAlgorithm=algorithm,
+        )
+        hmac = base64.b64encode(response["Mac"]).decode("utf-8")
+
+        return hmac
